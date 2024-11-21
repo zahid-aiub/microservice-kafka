09e5b56bc4429db6787e30515d53fd23ac3a9daf
Islam <MdZahidul.Islam@cbre.com>
2024-11-21 16:54:45 +0100

    services are created

---
+++ core-domain/src/main/java/com/core_domain/dto/Order.java
@@ -0,0 +1,48 @@
+package com.core_domain.dto;
+
+public class Order {
+
+    private String id;
+    private String orderStatus;
+    private int quantity;
+    private double price;
+
+    public Order(String id, String orderStatus, int quantity, double price) {
+        this.id = id;
+        this.orderStatus = orderStatus;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ core-domain/src/main/java/com/core_domain/dto/OrderEvent.java
@@ -0,0 +1,48 @@
+package com.core_domain.dto;
+
+public class OrderEvent {
+
+    private String id;
+    private String message;
+    private String status;
+    private Order order;
+
+    public OrderEvent(String id, String message, String status, Order order) {
+        this.id = id;
+        this.message = message;
+        this.status = status;
+        this.order = order;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+}
